961c5235a56d2c36deb8fd7b03617e76856eb28d
Waldorepe <67336923+Waldorepe@users.noreply.github.com>
2023-04-10 17:03:02 -0400

    Add files via upload

---
+++ Primer = input().py
@@ -0,0 +1,9 @@
+string_edad = input()                
+float_edad= float(string_edad)    
+int_edad= int(float_edad)      
+string_estatura = input()                
+float_estatura= float(string_estatura)    
+int_estatura= int(float_estatura)       
+print(int(float_edad) + float(string_estatura) )
+
+
